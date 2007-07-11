565df44bdabd8f288b4794f85b456d11011bc8f1
Anant Narayanan <anant@kix.in>
2007-07-11 05:37:08 +0500

    Web viewer now handles binary files.

--- php9p/examples/viewer.php
+++ php9p/examples/viewer.php
@@ -1,22 +1,26 @@
-<html>
-<head>
-	<title>9P File Viewer</title>
-</head>
-<body>
-
 <?php
 if (!isset($_REQUEST['address'])) {	
 ?>
+	<html>
+	<head>
+		<title>9P File Viewer</title>
+	</head>
+	<body>
+		<form name="ixp" method="get" action="viewer.php">
+		Address to Dial:
+			<input type="text" name="address" />
+			<input type="submit" value="View!" />
+		</form>
 	
-	<form name="ixp" method="get" action="viewer.php">
-	Address to Dial:
-		<input type="text" name="address" />
-		<input type="submit" value="View!" />
-	</form>
+		<p>
+			Example: tcp!sources.cs.bell-labs.com!564
+		</p>
 	
-	<p>
-	Example: tcp!sources.cs.bell-labs.com!564
-	</p>
+		<p>
+			Powered by <a href="http://code.kix.in/projects/web9">Web9</a>	
+		</p>
+	</body>
+	</html>
 
 <?php
 } else {
@@ -45,6 +49,7 @@ if (!isset($_REQUEST['address'])) {
 
 		ksort($names);
 
+		echo '<html><head><title>9P File Viewer</title></head><body>';
 		echo '<table width="80%">';
 		echo '<td></td>';
 		echo '<td><b>Name</b></td>';
@@ -73,19 +78,30 @@ if (!isset($_REQUEST['address'])) {
 		echo '</table>';
 		$client->unmount();
 		echo "<p>Browsing $address</p>";
+		echo '<p>Powered by <a href="http://code.kix.in/projects/web9">Web9</a></p>';
+		echo '</body></html>';
 
 	} else {
 
 		$cfid = $client->open($path);
+		$buf = $cfid->read($cfid->iounit);
+
+		$nam = tempnam(sys_get_temp_dir(), 'ixp');
+		$tmp = fopen($nam, 'w+');
+		fwrite($tmp, $buf);
+		$type = `file -i $nam`;
+		$type = explode(':', $type);
+		fclose($tmp);
+		unlink($nam);
+		
+		header('Content-Type: '.trim($type[1]));
+		echo $buf;
 		while ($buf = $cfid->read($cfid->iounit))
 			echo $buf;
+		
 		$cfid->close();
 		$client->unmount();
 
 	}	
 }
 ?>
-
-<p>Powered by <a href="http://code.kix.in/projects/web9">Web9</a></p>
-</body>
-</html>
